0dfc6913395fc761c6d6c0c1423d76b7d4a6bfa5
Kirill Tregubov <contact@kirilltregubov.com>
2023-09-21 17:30:49 -0400

    metadata icons not showing up in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,19 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'I am still here!',
+  // straight from https://nextjs.org/docs/app/api-reference/functions/generate-metadata#icons
+  icons: {
+    icon: '/icon.png',
+    shortcut: '/shortcut-icon.png',
+    apple: '/apple-icon.png',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: '/apple-touch-icon-precomposed.png',
+    },
+  },
+}
+
 export default function RootLayout({ children }) {
   return (
     <html>
